1557e8e8ffb2f1712cbf959122d6e13d5bb17ee4
DhvaniBhesaniya <narutogokuluffy009@gmail.com>
2024-06-06 15:09:23 +0530

    "Updated ExploreMenu component with new styles and functionality, added category state to Home component, and wrapped App with StoreContextProvider."

---
+++ frontend/src/context/StoreContext.jsx
@@ -0,0 +1,27 @@
+import { createContext, useState } from "react";
+import { food_list } from "../assets/frontend_assets/assets";
+
+export const StoreContext = createContext(null);
+const StoreContextProvider = (props) => {
+
+    const [cartItems,setCartItems] = useState({});
+const addtoCart =  (itemId) => {
+    if (!cartItems[itemId]) {
+        setCartItems((prev) => ({...prev,[itemId]:1}))
+    }
+    else{
+        setCartItems((prev) => ({...prev,[itemId]:prev[itemId]+1}))
+    }
+}
+
+  const contextValue = {
+    food_list,
+  };
+  return (
+    <StoreContext.Provider value={contextValue}>
+      {props.children}
+    </StoreContext.Provider>
+  );
+};
+
+export default StoreContextProvider;
