8dc2c05fb8a218b12639f17335b11763d38c5381
DhvaniBhesaniya <narutogokuluffy009@gmail.com>
2024-07-11 12:13:43 +0530

    got the same youtube code.

--- backend/config/db.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-  await mongoose
-    .connect(
-      "mongodb+srv://tomato:mytomatodb@gohan.kns9i2e.mongodb.net/food-delivery?retryWrites=true&w=majority&appName=gohan"
-    )
-    .then(() => console.log("DB connected"));
-};
