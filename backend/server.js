6ca96c08cfef950cc73e6801e1ffebde798b7942
DhvaniBhesaniya <narutogokuluffy009@gmail.com>
2024-07-25 14:17:27 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -57,10 +57,10 @@ app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRouter);
 
 
-// app.use(express.static(path.join(__dirname, '/frontend/dist')));
-// app.get('*',(req,res)=>{
-//   res.sendFile(path.join(__dirname, 'frontend','dist','index.html'));
-// })
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname, 'frontend','dist','index.html'));
+})
 
 app.get("/test", (req, res) => {
     res.send("API working");
