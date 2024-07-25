f3eba92420d5cf90f8673dd92dfaded1fc3afb88
DhvaniBhesaniya <narutogokuluffy009@gmail.com>
2024-07-25 14:13:54 +0530

    new change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,9 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    // host: false,
-    // staticPort: true,
-    // port: 5173,
+    build: {
+      outDir: 'dist',
+    },
     proxy:{
       '/api': {
        target:  'http://localhost:4000',
@@ -20,3 +20,27 @@ export default defineConfig({
     },
   },
 });
+
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     // host: false,
+//     // staticPort: true,
+//     // port: 5173,
+//     proxy:{
+//       '/api': {
+//        target:  'http://localhost:4000',
+//        secure:false,
+//       },
+//       '/image': {
+//         target: 'http://localhost:4000', // Adjust the target to your image server
+//         secure: false,
+//       },
+//     },
+//   },
+// });
